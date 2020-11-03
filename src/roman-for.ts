41594ef12493bf777be9e31278504d8031072803
Tom <thomas.spencer@codurance.com>
2020-11-03 19:38:08 +0000

    Add passing test for arabicFor

--- src/roman-for.ts
+++ src/roman-for.ts
@@ -18,3 +18,17 @@ export const romanFor = (input: number):string => {
   }
   return result;
 }
+
+export const arabicFor = (roman: string): number => {
+  let result = romanNumerals[roman.charAt(roman.length - 1)]
+
+  for (let i = roman.length - 2; i >= 0; i--){
+    let currentNumeral = romanNumerals[roman.charAt(i)]
+    if (currentNumeral < romanNumerals[roman.charAt(i + 1)]) {
+      result -= currentNumeral;
+    }else {
+      result += currentNumeral;
+    }
+  }
+  return result;
+};
--- test/roman-numeral.test.ts
+++ test/roman-numeral.test.ts
@@ -1,6 +1,6 @@
-import { romanFor } from "../src/roman-for";
+import {arabicFor, romanFor} from "../src/roman-for";
 
-describe("RomanNumerals", () => {
+xdescribe("RomanFor", () => {
   it.each([
     [1, "I"],
     [2, "II"],
@@ -10,8 +10,19 @@ describe("RomanNumerals", () => {
     [6, "VI"]
   ])(
     "converts arabic number to roman numeral",
-    (arabic, result) => {
-      expect(romanFor(arabic)).toEqual(result);
+    (arabic: number, roman: string) => {
+      expect(romanFor(arabic)).toEqual(roman);
     }
   );
+});
+
+describe("ArabicFor", () => {
+  it.each([
+    ["I", 1],
+    ["II", 2],
+    ["IV", 4],
+    ["VI", 6],
+  ])("converts roman to arabic", (roman: string, arabic: number) => {
+    expect(arabicFor(roman)).toEqual(arabic)
+  });
 });
