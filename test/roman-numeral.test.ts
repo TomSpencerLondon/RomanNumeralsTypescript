ca454d1b49598b809799f435ec099715728b809c
Tom <thomas.spencer@codurance.com>
2020-11-03 19:06:31 +0000

    Add passing test for III

--- test/roman-numeral.test.ts
+++ test/roman-numeral.test.ts
@@ -3,7 +3,8 @@ import { romanNumeral } from "../src/roman-numeral";
 describe("RomanNumerals", () => {
   it.each([
     [1, "I"],
-    [2, "II"]
+    [2, "II"],
+    [3, "III"]
   ])(
     "converts arabic number to roman numeral",
     (arabic, result) => {
